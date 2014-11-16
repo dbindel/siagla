7c4822311575a05700e0bdb92607f35bede3eade
David Bindel <bindel@cs.cornell.edu>
2014-11-16 12:26:09 -0500

    Added parser for scraping SIMAX RSS into _data/simax.yml.

---
+++ _util/parse-simax-feed.py
@@ -0,0 +1,69 @@
+#!/usr/local/bin/python
+
+from bs4 import BeautifulSoup
+import datetime
+import urllib2
+import sys
+import yaml
+
+DBNAME="simax.yml"
+URL="http://epubs.siam.org/action/showFeed?ui=0&mi=3ezuvv&ai=s7&jc=sjmael&type=etoc&feed=rss"
+
+def scrape(xml, dbname):
+
+    # Grab old database and mark DOIs to avoid repeat records
+    with open(dbname, 'r') as f:
+        ydoc = yaml.safe_load(f)
+    if ydoc == None:
+        ydoc = []
+        yset = frozenset([])
+    else:
+        yset= frozenset([yrec['doi'] for yrec in ydoc])
+
+    # Scrape the SIMAX RSS feed for information on new records
+    soup = BeautifulSoup(xml)
+    items = soup.find_all('rss:item')
+    for item in items:
+        title = item.find('rss:title')
+        link = item.find('rss:link')
+        doi = item.find('dc:identifier')
+        date = item.find('dc:date')
+        authors = date.next.next.encode("utf8")
+        volume = item.find('prism:volume')
+        pp_start = item.find('prism:startingpage')
+        pp_end   = item.find('prism:endingpage')
+        doitxt = doi.get_text().encode("utf8")
+        if doitxt not in yset:
+            print "Add: {0}".format(title.get_text().encode("utf8"))
+            ydoc.append({
+                'title': title.get_text().encode("utf8"),
+                'authors': authors,
+                'link': link.get_text().encode("utf8"),
+                'doi': doi.get_text().encode("utf8"),
+                'date': date.get_text().encode("utf8"),
+                'volume': volume.get_text().encode("utf8"),
+                'pp_start': pp_start.get_text().encode("utf8"),
+                'pp_end': pp_end.get_text().encode("utf8")})
+
+    # Sort all records and write updated database
+    ydoc.sort(key=lambda rec: int(rec['volume'])*1e6 + int(rec['pp_start']))
+    with open(dbname, 'w') as f:
+        f.write('# Automatically generated from SIMAX RSS feed\n')
+        f.write("# See parse-simax-feed.py\n\n")
+        yaml.safe_dump(ydoc, stream=f, default_flow_style=False, encoding='utf-8', allow_unicode=True)
+
+def main(url, dbname):
+    response = urllib2.urlopen(url)
+    xml = response.read()
+    scrape(xml, dbname)
+
+def mainf(fname, dbname):
+    with open(fname, 'r') as f:
+        xml = f.read()
+    scrape(xml, dbname)
+    
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        main(URL, DBNAME)
+    elif len(sys.argv) == 2:
+        main(URL, sys.argv[1])
